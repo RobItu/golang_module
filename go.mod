4cb462916bb182618d72a8e0df95812152edfefd
robitu <robertoiturralde2009@hotmail.com>
2025-05-03 02:33:25 -0600

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RobItu/golang_module.git
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func task(name string) {
+	fmt.Println("Starting task:", name)
+	time.Sleep(1 * time.Second)
+	fmt.Println("Finished task:", name)
+}
+
+func main() {
+	go task("Alpha")
+	go task("Beta")
+	go task("Charlie")
+
+	time.Sleep(3 * time.Second)
+	fmt.Println("Main function completed.")
+}
